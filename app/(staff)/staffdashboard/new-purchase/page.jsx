00e625de9302d97b985f63502b5987eab37abf90
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2025-05-21 11:48:43 +0300

    Submit color change

--- app/(staff)/staffdashboard/new-purchase/page.jsx
+++ app/(staff)/staffdashboard/new-purchase/page.jsx
@@ -120,7 +120,7 @@ export default function NewPurchase() {
             className={`mx-auto my-8 block cursor-pointer rounded-xl px-6 py-3 text-lg text-white transition-colors ${
               isSubmitting
                 ? "cursor-not-allowed bg-gray-400"
-                : "bg-green-600 hover:bg-green-700"
+                : "bg-red-800 hover:bg-red-900"
             }`}
           >
             {isSubmitting ? "Submitting..." : "Submit Form"}
