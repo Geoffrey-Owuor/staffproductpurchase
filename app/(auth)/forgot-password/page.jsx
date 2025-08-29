344ef198a27f2f33ec6922a0ad2077573200580f
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2025-08-29 15:37:52 +0300

    fixed image error in /forgot-password

--- app/(auth)/forgot-password/page.jsx
+++ app/(auth)/forgot-password/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { HotpointSvgLogo } from "@/public/assets";
 
 export default function ForgotPassword() {
