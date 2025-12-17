dbc2e4c9a13a656afffba069205c9b73f0d5316f
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2025-12-17 15:49:56 +0300

    Added a new changelog page

---
+++ app/(changelog)/changelog/page.jsx
@@ -0,0 +1,19 @@
+import ChangeLogRoute from "@/components/ChangeLog/ChangeLogRoute";
+import Header from "@/components/landingpage/Header";
+import { Suspense } from "react";
+import ChangelogSkeleton from "@/components/skeletons/ChangeLogSkeleton";
+import PagesFooter from "@/components/Reusables/PagesFooter/PagesFooter";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <Suspense fallback={<ChangelogSkeleton />}>
+        <ChangeLogRoute />
+      </Suspense>
+      <PagesFooter />
+    </>
+  );
+};
+
+export default page;
--- components/usermanual/UserManual.jsx
+++ components/usermanual/UserManual.jsx
@@ -7,11 +7,10 @@ import {
   LifeBuoy,
   CheckCircle2,
   CreditCard,
-  BrainCog,
   Banknote,
   AlertCircle,
 } from "lucide-react";
-import ThemeToggle from "../Reusables/ThemeProviders/ThemeToggle";
+import PagesFooter from "../Reusables/PagesFooter/PagesFooter";
 import Header from "../landingpage/Header";
 
 // --- STATIC DATA ---
@@ -189,28 +188,8 @@ const UserManual = () => {
           </div>
         </div>
       </section>
-      <div className="relative px-10 py-14 dark:border-gray-800">
-        {/* Centered Part */}
-        <div className="absolute inset-0 top-0 flex items-center justify-center space-x-1 text-sm md:top-7">
-          <span className="text-gray-500 dark:text-gray-400">
-            © {new Date().getFullYear()} Hotpoint Appliances Ltd. Built by
-          </span>
-          <a
-            href="https://jeff-portfolio-web.vercel.app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center gap-1 text-gray-500 hover:text-gray-600 hover:underline dark:hover:text-gray-400"
-          >
-            <span className="font-semibold">Jeff</span>
-            <BrainCog className="h-3.5 w-3.5" />
-          </a>
-        </div>
-
-        {/* ThemeToggle pinned right */}
-        <div className="absolute top-18 right-4 hidden -translate-y-1/2 sm:block">
-          <ThemeToggle />
-        </div>
-      </div>
+      {/* Footer */}
+      <PagesFooter />
     </>
   );
 };
