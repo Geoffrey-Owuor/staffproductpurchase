324d7dcef56cf3b288cccaa0209bbea53fcc8aef
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2025-12-18 14:12:40 +0300

    add revalidate to changelog route path

--- app/(changelog)/changelog/page.jsx
+++ app/(changelog)/changelog/page.jsx
@@ -1,3 +1,5 @@
+export const revalidate = 8400; //Refresh Fallback - revalidate after 24 hrs
+
 import ChangeLogRoute from "@/components/ChangeLog/ChangeLogRoute";
 import Header from "@/components/landingpage/Header";
 import { Suspense } from "react";
