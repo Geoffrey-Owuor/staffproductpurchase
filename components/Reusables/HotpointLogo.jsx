f06d6619175861731c3700097c94779e53f06e65
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2025-12-22 14:56:57 +0300

    Adding x-mas festivity icons

--- components/Reusables/HotpointLogo.jsx
+++ components/Reusables/HotpointLogo.jsx
@@ -1,7 +1,14 @@
 import { ShoppingBagIcon } from "lucide-react";
+import { assets } from "@/public/assets";
+import Image from "next/image";
 export default function HotpointLogo({ isOpen }) {
   return (
-    <div className="flex items-center gap-1">
+    <div className="relative flex items-center gap-1">
+      <Image
+        src={assets.hat}
+        alt="christmas hat"
+        className="absolute bottom-2.5 left-5.5 w-6"
+      />
       <ShoppingBagIcon className="h-5 w-5 text-gray-900 dark:text-white" />
       <span
         className={`overflow-hidden text-xl font-semibold whitespace-nowrap text-gray-900 ${isOpen ? "custom:w-30" : "custom:w-0"} transition-all duration-200 dark:text-white`}
