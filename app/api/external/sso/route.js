ac95900c4a6efd85f87ba2b6b971c3dbcf3031d0
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2026-06-15 20:37:12 +0300

    removing error console logs which are no longer required[C

--- app/api/external/sso/route.js
+++ app/api/external/sso/route.js
@@ -71,7 +71,6 @@ export async function GET(request) {
 
     // No user found
     if (user.length === 0) {
-      console.log("Could not find the selected user");
       return NextResponse.redirect(new URL("/login", baseUrl));
     }
 
